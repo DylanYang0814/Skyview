d0ae51b88468981646120f11da46d2d7d24e24e8
BT_1N3_11 <cyang4980@bths.edu>
2018-11-27 10:14:50 -0500

    Runner

--- src/Skyview.java
+++ src/Skyview.java
@@ -4,29 +4,41 @@ public class Skyview {
 
     public Skyview(int numRows, int numCols, double[] scanned) {
         int num = 0;
-        double[][] view = new double[numRows][numRows];
+        this.view = new double[numRows][numRows];
         for (int r = 0; r < numRows; r++) {
             for (int c = 0; c < numCols; c++) {
                 if (r % 2 == 0) {
                     view[r][c] = scanned[num];
                 } else {
-                    view[r][numCols - c] = scanned[num];
+                    view[r][numCols - c - 1] = scanned[num];
                     num++;
                 }
             }
         }
     }
 
-    public double getAverage(int startRow, int endRows, int startcol, int endCol) {
-        int sum = 0;
+    public double getAverage(int startRow, int endRow, int startCol, int endCol) {
+        double sum = 0;
         int count = 0;
 
-        for (int r = startRow; r < endRows; r++) {
-            for (int c = startcol; c < endCol; c++) {
-                sum += view[r][c];
+        for (int row = startRow; row <= endRow; row++) {
+            for (int col = startCol; col <= endCol; col++) {
+                sum += view[row][col];
                 count++;
             }
         }
-        return sum/count;
+
+        return sum / count;
+    }
+
+    public String toString() {
+        String out = "";
+        for (int i = 0; i < view.length; i++) {
+            for (int j = 0; j < view[i].length; j++) {
+                out += "[" + view[i][j] + "]";
+            }
+            out += "\n";
+        }
+        return out;
     }
 }
